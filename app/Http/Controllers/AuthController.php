9e9134d30a9cc83de1a3ea724e3d1f95f517362e
Gerson Kilderys de Oliveira <gersonkilderys@gmail.com>
2022-06-03 23:40:04 -0300

    pagina de login admin com laravel

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function dashboard()
+    {
+        if (Auth::check() === true) {
+
+            return view('admin.dashboard');
+        }
+        return redirect()->route('admin.login');
+    }
+    public function showLoginForm()
+    {
+        return view('admin.formLogin');
+    }
+    public function login(Request $request)
+    {
+        if (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
+            return redirect()->back()->withInput()->withErrors(['O e-mail informado não é válido!']);
+        }
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('admin');
+        }
+        return redirect()->back()->withInput()->withErrors(['Os dados informados não conferem!']);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('admin');
+    }
+}
+//https://www.youtube.com/watch?v=J26cSCF9De4&ab_channel=RobsonV.Leite
